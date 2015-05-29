94a9dbabf6b3ae3ce915b76a5a195fdb15aada7c
Evgenii Seliavka <eseliavka@griddynamics.com>
2015-05-29 19:37:17 +0300

    extract time fields

--- src/replephant/getJobData.clj
+++ src/replephant/getJobData.clj
@@ -1,5 +1,8 @@
 (def jobs (load-jobs "/local/path/to/hadoop/job-history-root-dir"))
-(defn getJobData [user] (map #(vals %) (map #(select-keys % [:mapred.reduce.tasks, :mapred.map.tasks, :submit-time]) (filter #(= (:user.name % 0) user) jobs))))
+(defn getJobData [user] (map #(vals %) (map #(select-keys % [:mapred.reduce.tasks, :mapred.map.tasks, :mapred.job.name, :finish-time, :launch-time, :submit-time]) (filter #(= (:user.name % 0) user) jobs))))
 (def mrdata (getJobData "eselyavka"))
 (use 'clojure.java.io)
-(with-open [wrtr (writer "/tmp/test.txt")](doseq [i mrdata] (.write wrtr (apply str (interpose ", " (cons (quot (first i ) 1000) (rest i) ) ) ))(.write wrtr "\n")))
+;write csv file epoch with ms
+(with-open [wrtr (writer "/tmp/test.txt")](doseq [i mrdata] (.write wrtr (apply str (interpose ";"  i) ))(.write wrtr "\n")))
+;write csv file, submit time is epoch without ms
+;(with-open [wrtr (writer "/tmp/test.txt")](doseq [i mrdata] (.write wrtr (apply str (interpose ";" (cons (quot (first i ) 1000) (rest i) ) ) ))(.write wrtr "\n")))
